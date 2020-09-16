fa2f85b8a684e5f2a526573ea12e269f88c5b97a
matheushss1 <matheushenriquedesouzasilva@hotmail.com>
2020-09-16 15:16:33 -0300

    Teste com app 2

---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <h2>Social</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
