92b333ebf979e4c3bc99d3a3c43aa1933455a7fd
matheushss1 <matheushenriquedesouzasilva@hotmail.com>
2020-09-18 16:30:02 -0300

    criado navbar dinamica e adicionado materialize para css/js

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
-    <router-view/>
+    <nav-bar cor='blue' />
+    <router-view />
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/layouts/NavBar'
 export default {
-  name: 'App'
-}
+  name: "app",
+  components: {
+    NavBar
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
