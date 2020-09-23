a3898311ce3168e5a029a8c5b17c1d527d1cf912
matheushss1 <matheushenriquedesouzasilva@hotmail.com>
2020-09-23 15:57:45 -0300

    criada validacao

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+
+Route::post('/cadastro', function (Request $request){
+    $data = $request->all();
+    $validacao = Validator::make($data, [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed',
+    ]);
+    if($validacao->fails()){
+        return $validacao->errors();
+    }
+
+    $user = User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => $data['password'],
+    ]);
+    $user->token = $user -> createToken($user->email)->accessToken;
+
+    return $user;
+});
+
+Route::middleware('auth:api')->get('/usuario', function (Request $request) {
     return $request->user();
 });
