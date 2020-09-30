65c70b73b952e54915aff2afe36ab89588c1224d
matheushss1 <matheushenriquedesouzasilva@hotmail.com>
2020-09-30 16:21:00 -0300

    realizados todos os testes com condicionais e criado um joguinho para medir aprendizado heheheeheheh

--- condicionais/app.js
+++ condicionais/app.js
@@ -1,6 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+       enteredGoalValue: '',
+       goals: []
+      };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    },
+    removeGoal(idx){
+      this.goals.splice(idx, 1);
+    }
   },
 });
 
